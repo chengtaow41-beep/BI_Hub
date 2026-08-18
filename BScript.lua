f331a98143fe5147a08f679b900fd1a30287b949
chengtaow41-beep <chengtaow41@gmail.com>
2026-08-18 14:58:13 +0800

    更新 BScript.lua

--- BScript.lua
+++ BScript.lua
@@ -1,12 +1,12 @@
 local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Footagesus/WindUI/refs/heads/main/dist/main.lua"))()
 local PAGES={
-  currentPage="公告"
+  currentPage = "公告"
 --这里你可以自己定义
-  container=nil,
+  container = nil,
 --页面容器，所有页面放这里
-  pages={},
+  pages = {},
 --储存所有页面创建函数
-  callbacks={}
+  callbacks = {}
 --页面存在周期回调
 }
 

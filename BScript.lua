abf0628d3e1d4828fe46be5569024f6807ba3c64
chengtaow41-beep <chengtaow41@gmail.com>
2026-08-18 17:36:44 +0800

    更新 BScript.lua

--- BScript.lua
+++ BScript.lua
@@ -104,6 +104,24 @@ local MainTab = Window:Tab({
      Title = "公告",
      Icon = "megaphone"
  })
+-- 大标题卡片
+ InfoTab:Section({
+     Title = "风脚本",
+     Desc = "兄弟们，这个脚本是免费的，有倒卖的在抖音群说一下！"
+ })
+ -- QQ群卡片，点击复制群号
+ InfoTab:Button({
+     Title = "风脚本主群",
+     Desc = "待定",
+     Callback = function()
+         setclipboard("群号待定")
+         WindUI:Notify({
+             Title = "待定",
+             Content = "粘贴即可去加群",
+             Duration = 2
+         })
+     end
+ })
  local MiscTab = Window:Tab({
      Title = "通用",
      Icon = "settings"

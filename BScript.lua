6d4bb374dbbc2b358688bcee37fdb834066e6d25
chengtaow41-beep <chengtaow41@gmail.com>
2026-08-18 18:03:25 +0800

    更新 BScript.lua

--- BScript.lua
+++ BScript.lua
@@ -104,26 +104,11 @@ local AnnounceTab = Window:Tab({
     Title = "公告",
     Icon = "megaphone"
 })
-AnnounceTab:Button({
-     Title = "风脚本主群",
-     Desc = "待定",
-     Callback = function()
-         setclipboard("114514")
-         WindUI:Notify({
-             Title = "已复制KS群号",
-             Content = "直接粘贴即可加群",
-             Duration = 2
-         })
-     end
 AnnounceTab:Section({
     Title = "风脚本",
     Desc = "永久免费\n公益脚本禁止倒卖，认准风脚本"
 })
-AnnounceTab:Section({
-     Title = "风脚本 v1.0.0.0 更新",
-     Desc = "完成UI基础框架，自动防挂机，公告页面"
-     
-})
+
  local MiscTab = Window:Tab({
      Title = "通用",
      Icon = "settings"
